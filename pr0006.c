19cda0cbc96b79e5a97ef329154b6b044dd90e1c
martynova <chamomiletoad.sandalwoodwhale@gmail.ru>
2025-04-21 15:25:37 +0700

    Добавили вывод значений перменных среды, добавили условия изменения поведения программы

--- pr0006.c
+++ pr0006.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 int main(int argc, char* argv[], char* envp[]) {
-	printf("Количество аргументов, переданных программ :%d\n", argc);
-	printf("\n");
-	for (int i=0; i <argc; i++)
-		printf("Аргумент[%d]: %s\n", i, argv[i]);
-	printf("\n");	
+	if (argc >= 2) {
+		printf("Количество аргументов, переданных программ :%d\n", argc);
+		printf("\n");
+		for (int i=0; i <argc; i++)
+			printf("Аргумент[%d]: %s\n", i, argv[i]);
+		printf("\n");
+		}	
+		
+		else {
+			printf("Список перемнных среды, в которой запущенная прогрмамма: \n");
+			
+	
+			int j=0;	
+
+			while (envp[j]!=NULL) {
+				printf("%d %s\n", j, envp[j]);
+				j++;
+	}
+	}
 	return 0;
 }
